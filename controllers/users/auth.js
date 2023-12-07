15581fb3c62f1f67c8913542eb13c9033b40d1cb
vishal <jakharvishalv@gmail.com>
2023-12-07 13:17:47 +0530

    sdsd

--- controllers/users/auth.js
+++ controllers/users/auth.js
@@ -29,7 +29,7 @@ const setCookies = (res, data) => {
   //Secure the cookie (check if it was set with the domain mentioned and not locally)
   const opts = IS_PRODUCTION
     ? {
-      domain: "esummit-iitm.netlify.app",  // Update this with your Netlify domain
+
       secure: true,  // Set this to true since Netlify uses HTTPS
       sameSite: "None"
     }
